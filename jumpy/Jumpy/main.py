af6603b4dbc5a5bcac75a6c0cf4d2fbd69dff334
Bach Linh Vo <linh06102004@gmail.com>
2024-02-12 20:01:41 -0800

    implemented game over and start over method and infinite platform

--- jumpy/Jumpy/main.py
+++ jumpy/Jumpy/main.py
@@ -3,14 +3,19 @@ import random
 #intialization
 pygame.init()
 #constants
-SCREEN_WIDTH = 1280
-SCREEN_HEIGHT = 768
-MAX_PLATFORM = 40
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 600
+MAX_PLATFORM = 10
 WHITE = (255, 255, 255)
 GRAVITY = 1
 SCROLL_THRESH = 300
 scroll = 0
 bg_scroll = 0
+game_over = False
+score = 0
+
+font_small = pygame.font.SysFont('Lucida Sans', 20)
+font_big = pygame.font.SysFont('Lucida Sans', 24)
 #game window
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("jumping bullet hell")
@@ -22,11 +27,15 @@ FPS = 60
 player_image = pygame.image.load("asset/idle1.png").convert_alpha()
 bg = pygame.image.load("asset/bg.jpg").convert_alpha()
 platform_image = pygame.image.load("asset/pad.png").convert_alpha()
-bg = pygame.transform.scale(bg,(1280,768))
+bg = pygame.transform.scale(bg,(400,600))
+
+def draw_text(text, font, text_color, x, y):
+    img = font.render(text, True, text_color)
+    screen.blit(img, (x,y))
 
 def draw_bg(bg_scroll):
     screen.blit(bg, (0,0 + bg_scroll))
-    screen.blit(bg, (0, -768 + bg_scroll))
+    screen.blit(bg, (0, -600 + bg_scroll))
 
 
 
@@ -89,12 +98,6 @@ class Player:
                         self.vel_y = 0
                         self.on_ground = True
 
-        if self.rect.bottom + dy > SCREEN_HEIGHT:
-            dy = 0
-            #self.vel_y = -16
-            self.vel_y = 0
-            self.on_ground = True
-
         if self.rect.top <= SCROLL_THRESH:
             if self.vel_y < 0:
                 scroll = -dy
@@ -119,34 +122,58 @@ class Platform(pygame.sprite.Sprite):
     def update(self,scroll):
 
         self.rect.y += scroll
+        if self.rect.top > SCREEN_HEIGHT:
+            self.kill()
 
 platform_group = pygame.sprite.Group()
 player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT - 150, player_image)
 
-
-for p in range(MAX_PLATFORM):
-    p_w = random.randint(50,100)
-    p_x = random.randint(0, SCREEN_WIDTH - p_w)
-    p_y = random.randint(100, SCREEN_HEIGHT - 80)
-    platform = Platform(p_x, p_y, p_w, platform_image)
-    platform_group.add(platform)
+platform = Platform(SCREEN_WIDTH // 2 - 50, SCREEN_HEIGHT -120, 100, platform_image)
+platform_group.add(platform)
 
 run = True
 while run:
 
     clock.tick(FPS)
+    if game_over == False:
+        scroll = player.move()
 
-    scroll = player.move()
+        bg_scroll += scroll
+        if bg_scroll >= 768:
+            bg_scroll = 0
+        draw_bg(bg_scroll)
 
-    bg_scroll += scroll
-    if bg_scroll >= 768:
-        bg_scroll = 0
-    draw_bg(bg_scroll)
+        platform_group.draw(screen)
+        player.draw(screen)
 
-    platform_group.draw(screen)
-    player.draw(screen)
-    pygame.draw.line(screen, WHITE, (0, SCROLL_THRESH), (SCREEN_WIDTH, SCROLL_THRESH))
-    platform_group.update(scroll)
+        if player.rect.top > SCREEN_HEIGHT:
+            game_over = True
+#    pygame.draw.line(screen, WHITE, (0, SCROLL_THRESH), (SCREEN_WIDTH, SCROLL_THRESH))
+
+        if len(platform_group) < MAX_PLATFORM:
+            p_w = random.randint(50, 100)
+            p_x = random.randint(0,SCREEN_WIDTH - p_w)
+            p_y = platform.rect.y - random.randint(80, 120)
+            platform = Platform(p_x, p_y, p_w, platform_image)
+            platform_group.add(platform)
+
+
+
+        platform_group.update(scroll)
+
+    else:
+        draw_text('GAME OVER! ', font_big, WHITE, 125, 200)
+        draw_text('SCORE: ' + str(score), font_big, WHITE, 130, 250)
+        draw_text('PRESS O TO PLAY AGAIN', font_big, WHITE, 40, 300)
+        key = pygame.key.get_pressed()
+        if key[pygame.K_o]:
+            game_over = False
+            score = 0
+            scroll = 0
+            player.rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT - 150)
+            platform_group.empty()
+            platform = Platform(SCREEN_WIDTH // 2 - 50, SCREEN_HEIGHT - 120, 100, platform_image)
+            platform_group.add(platform)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
